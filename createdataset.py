674020027054b8a2ec139145479f51d9848da35a
Loweryder <ryan.lowe_@hotmail.com>
2015-04-25 21:22:42 -0400

    added preventative measures

--- createdataset.py
+++ createdataset.py
@@ -53,6 +53,13 @@ def getUserList(c2):
       userlist.append(row[1])
   return userlist  
 
+def is_number(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
+
 def checkValidity(c2, percentage, convo): #checks whether we accept or reject a file
   #c2 = c1.read().split('\n')
   userlist = []
@@ -75,10 +82,16 @@ def checkValidity(c2, percentage, convo): #checks whether we accept or reject a 
 def diff_times_in_seconds(t1,t2,date1,date2):
   t1 = t1.split(':')
   t2 = t2.split(':')
-  h1,m1,s1 = int(t1[0]),int(t1[1]),0#int(t1[2])
-  h2,m2,s2 = int(t2[0]),int(t2[1]),0#int(t2[2])
   date1 = date1.split('-')
   date2 = date2.split('-')
+  if len(t1)<2 or len(t2)<2 or len(date1)<3 or len(date2)<3:
+    return 60*60*24 #return 1 day if something goes wrong
+  if not is_number(t1[0]) or not is_number(t1[1]) or not is_number(t2[0]) or not is_number(t2[1]):
+    return 60*60*24
+  if not is_number(date1[0]) or not is_number(date1[1]) or not is_number(date1[2]) or not is_number(date2[0]) or not is_number(date2[1]) or not is_number(date2[2]):
+    return 60*60*24
+  h1,m1,s1 = int(t1[0]),int(t1[1]),0#int(t1[2])
+  h2,m2,s2 = int(t2[0]),int(t2[1]),0#int(t2[2])
   d1,mo1,yr1 = int(date1[2]),int(date1[1]),int(date1[0])
   d2,mo2,yr2 = int(date2[2]),int(date2[1]),int(date2[0])
   t1_secs = s1 + 60*(m1 + 60*(h1 + 24*(d1+ 30*(mo1+12*yr1))))
@@ -158,7 +171,7 @@ def replace_sentence(text):
         if not changed:
           if word.startswith('http://') or word.startswith('https://'):
               sentence.append("**URL**")
-          elif word.isdigit():
+          elif is_number(word):
               sentence.append("**NUMBER**")
           elif os.path.isabs(word):
               sentence.append("**PATH**")
@@ -205,9 +218,9 @@ class CreateDataset:
     while len(lastval.split('T')) <2:
       lastind -= 1
       lastval = c2[lastind].split('\t')[0]    
-    if len(firstval.split('T')) < 2 or len(lastval.split('T')) <2:
-      print firstval
-      print lastval
+    #if len(firstval.split('T')) < 2 or len(lastval.split('T')) <2:
+    #  print firstval
+    #  print lastval
     firstdate = firstval.split('T')[0]
     firsttime = firstval.split('T')[1].split('Z')[0]
     lastdate = lastval.split('T')[0]
